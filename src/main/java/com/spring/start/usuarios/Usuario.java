6eec98a5549acf4554ae0e7956c86e6b03ed896d
Sergioig37 <sergioig37@educastur.es>
2024-02-07 12:27:25 +0100

    Cambios

--- src/main/java/com/spring/start/usuarios/Usuario.java
+++ src/main/java/com/spring/start/usuarios/Usuario.java
@@ -1,10 +1,15 @@
 package com.spring.start.usuarios;
 
+import java.util.Collection;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 
 @Entity
-public class Usuario {
+public class Usuario implements UserDetails{
 
 	
 	@Id
@@ -22,6 +27,38 @@ public class Usuario {
 	}
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	@Override
+	public String getUsername() {
+		// TODO Auto-generated method stub
+		return usuario;
+	}
+	@Override
+	public boolean isAccountNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		// TODO Auto-generated method stub
+		return true;
+	}
+	@Override
+	public boolean isEnabled() {
+		// TODO Auto-generated method stub
+		return true;
 	}
 	
 	
